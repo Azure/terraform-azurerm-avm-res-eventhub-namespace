bdac3815157e4b7b6dc6252184a2ba1855fd3877
mbilalamjad <79409563+mbilalamjad@users.noreply.github.com>
2025-04-28 03:08:56 -0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -26,6 +26,7 @@ locals {
 # Helper locals to make the dynamic block more readable
 # There are three attributes here to cater for resources that
 # support both user and system MIs, only system MIs, and only user MIs
+# tflint-ignore: terraform_unused_declarations
 locals {
   managed_identities = {
     system_assigned_user_assigned = (var.managed_identities.system_assigned || length(var.managed_identities.user_assigned_resource_ids) > 0) ? {
